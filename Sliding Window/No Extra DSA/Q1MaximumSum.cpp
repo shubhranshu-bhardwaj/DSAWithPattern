6c19064eb1fb93d836a778d33b578566d8d97ef9
Shubhranshu Bhardwaj <bhardwajshubhranshu@gmail.com>
2025-09-12 22:28:31 +0530

    Sliding Window with No extra DSA

---
+++ Sliding Window/No Extra DSA/Q1MaximumSum.cpp
@@ -0,0 +1,10 @@
+/*
+Q1. Maximum Sum Subarray of Size K (LC 643)
+Given an array of integers and an integer k, find the maximum sum of a contiguous subarray of size k.
+Example 1:
+Input: arr = [2,1,5,1,3,2], k = 3
+Output: 9
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
